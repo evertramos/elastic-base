7b2dc232871d4f1d89a3dc2f35a98ed4a00205c9
Evert Ramos <evert.ramos@cgu.gov.br>
2021-06-06 22:59:22 -0300

    Fix web.php path

--- src/App/Providers/ElasticBaseServiceProvider.php
+++ src/App/Providers/ElasticBaseServiceProvider.php
@@ -8,7 +8,7 @@ class ElasticBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
     }
 
     public function register()
